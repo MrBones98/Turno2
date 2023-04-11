eb984e2c292676b02467617b8161cc6bf0d8f926
Pete Kolpakov <pete.kolpakov@gmail.com>
2023-04-11 13:28:30 +0300

    Added Main Menu placeholder
    added main manu scene and main menu handler script

---
+++ Assets/Scripts/Menus/MainMenuHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public void StartGame()
+    {
+    print("Start Game Button Pressed");
+    }
+}
