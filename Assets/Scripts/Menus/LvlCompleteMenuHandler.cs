b576003b56484f5a5e49faf7df6d25bc7262d808
Pete Kolpakov <pete.kolpakov@gmail.com>
2023-04-12 20:21:40 +0300

    pause + lvl complete menus
    added placeholder prefabs and scripts

---
+++ Assets/Scripts/Menus/LvlCompleteMenuHandler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LvlCompleteMenuHandler : MonoBehaviour
+{
+    public void GoToMainMenuButtonPressed()
+    {
+        print("Go To Main Menu Button Pressed");
+    }
+    
+    public void ResetButtonPressed()
+    {
+        print("Reset Button Pressed");
+    }
+    
+    public void NextLvlButtonPressed()
+    {
+        print("Next Level Button Pressed");
+    }
+}
---
+++ Assets/Scripts/Menus/PauseMenuHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuHandler : MonoBehaviour
+{
+
+    public void ContinueButtonPressed()
+    {
+        print("Continue Button Pressed");
+    }
+
+    public void GoToMainMenuButtonPressed()
+    {
+        print("Go To Main Menu Button Pressed");
+    }
+    
+    public void ResetButtonPressed()
+    {
+        print("Reset Button Pressed");
+    }
+
+        public void QuitButtonPressed()
+    {
+        print("Quit Button Pressed");
+    }
+}
