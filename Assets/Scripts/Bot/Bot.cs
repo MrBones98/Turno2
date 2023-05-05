b591fe2aa840e0a3537344bc119a446c191b33af
MrBones98 <salasdaniel1998@gmail.com>
2023-05-05 16:56:19 +0200

    First step of the async refactoring

--- Assets/Scripts/Bot/Bot.cs
+++ Assets/Scripts/Bot/Bot.cs
@@ -17,8 +17,12 @@ public class Bot : MonoBehaviour
     private int _stepCount;
     private bool _grounded = true;
     private bool _isActive = true;
+    private bool _platformCached = false;
     private Vector3 _lookDirection;
+    private Vector3 _movementDirection;
     private Vector3 _gizmoPosition;
+    private WallTile _wallTile= null;
+    private PushableBox _pushableBox = null;
 
     public delegate void OnFinishedMove();
     public static OnFinishedMove onFinishedMove;
@@ -29,12 +33,13 @@ public class Bot : MonoBehaviour
     }
     private void Awake()
     {
-        SolveTurnAsync();
+        //SolveTurnAsync();
     }
     public void CheckMove(Vector3 direction)
     {
         if (_isActive)
         {
+            _movementDirection = direction;
             Vector3 correctedDirection = direction.normalized;
             _gizmoPosition = correctedDirection;
             _lookDirection = (_parentGameObject.transform.position + direction) - _parentGameObject.transform.position;
@@ -72,11 +77,95 @@ public class Bot : MonoBehaviour
                 StartCoroutine(SolveTurn(correctedDirection));
         }
     }
-    async void SolveTurnAsync()
+    async Task SolveCollisionsAsync(Vector3 direction)
     {
-        print(Time.captureFramerate);
+        //Raycast check
         await Task.Yield();
-        print(Time.captureFramerate);
+
+        RaycastHit[] facingHit = Physics.RaycastAll(_parentGameObject.transform.position, _parentGameObject.transform.forward, 1f);
+        
+        for (int i = 0; i < facingHit.Length; i++)
+        {
+            if(_platformCached==false && facingHit[i].collider.gameObject.layer == 7)
+            {
+                _platformCached = true;
+            }
+            if (facingHit[i].collider.gameObject.GetComponent<WallTile>())
+            {
+                _wallTile = facingHit[i].collider.gameObject.GetComponent<WallTile>();
+            }
+            else if(_wallTile == null)
+            {
+                _wallTile=null;
+            }
+            if(_pushableBox== null || facingHit[i].collider.gameObject.GetComponent<PushableBox>())
+            {
+                _pushableBox = facingHit[i].collider.gameObject.GetComponent<PushableBox>();
+            }
+            print(facingHit[i].collider.gameObject.layer.ToString());
+        }
+        //await solve collisions to move
+
+    }
+    async Task SolveMovementAsync(WallTile walltile,bool platformCached, PushableBox pushablebox, Vector3 direction)
+    {
+        print($"In the {direction} direction there are: WallTile = {walltile}, Box = {pushablebox}, Platform in front = {platformCached}");
+        if(walltile == null || walltile!= null && walltile.HasColision)
+        {
+            if(platformCached)
+            {
+                _grounded = true;
+            }
+            else 
+            {
+                _grounded=false;
+            }
+            if(pushablebox != null)
+            {
+                //Var CheckMoveDirection ASYNC from bot
+                //await ^^/ can add bool return value for box.IsPushable
+                
+                if (true)
+                {
+                    Debug.LogWarning("Add Box Direction Check");
+                }
+                else
+                {
+                    //Move Bot with direction
+                }
+            }
+            else
+            {
+                //Move Bot with direction
+            }
+            if (_grounded == false)
+            {
+                //do another await for if (box=> will be platform/became platform)
+                //Dead Anim
+                print("No platform underneath Bot => Death after movement");
+            }
+        }
+        else
+        {
+            print($"No movement in {direction}, a wall blocks the path!");
+        }
+    }
+    async void SolveTurnAsync(Vector3 direction)
+    {
+
+        while(_stepCount > 0)
+        {
+            var solveCollisionsTask = SolveCollisionsAsync(direction);
+            await solveCollisionsTask;
+            var solveMovementTask = SolveMovementAsync(_wallTile,_platformCached, _pushableBox, direction);
+            await solveMovementTask;
+        }
+        //Move await=>loop through hits
+        onFinishedMove();
+        
+        //so 2 functions for resolution
+        //1 for check move (the one that has both calls for them and awaits move result before going through the next bot._stepCount
+        
         //allrighty
 
     }
@@ -92,6 +181,32 @@ public class Bot : MonoBehaviour
         while(_stepCount > 0)
         {
 
+            RaycastHit[] facingHit =Physics.RaycastAll(_raycastOrigin.position,_raycastOrigin.forward,0.8f);
+            yield return new WaitForSeconds(_botStepDelay);
+            print($"There are {facingHit.Length} colliders on this step");
+            for(int i = 0; i < facingHit.Length; i++)
+            {
+                if (platformCheck== false && facingHit[i].collider.gameObject.layer ==7)
+                {
+                    print(facingHit[i].collider.gameObject.layer.ToString());
+                    platformCheck = true;
+                }
+                
+                //print(i);
+                if (facingHit[i].collider.gameObject.GetComponent<WallTile>())
+                {
+                    wallTile = facingHit[i].collider.gameObject.GetComponent<WallTile>();
+                }
+                else if(wallTile == null)
+                {
+                    wallTile = null;
+                }
+                if(box == null || facingHit[i].collider.gameObject.GetComponent<PushableBox>())
+                {
+                    box = facingHit[i].collider.gameObject.GetComponent<PushableBox>();
+                }
+                  print(facingHit[i].collider.gameObject.name); 
+            }
             ////if (Physics.Raycast(_parentGameObject.transform.position, _parentGameObject.transform.TransformDirection(Vector3.forward), out facingHit, 1))
             //if(Physics.Raycast(_parentGameObject.transform.position, _parentGameObject.transform.forward, out facingHit,1 ,interactableLayers))
                 //var collision = facingHit.collider.gameObject;
@@ -123,33 +238,7 @@ public class Bot : MonoBehaviour
             //}
             //print(box);
             //RaycastHit[] facingHit =Physics.RaycastAll(_parentGameObject.transform.position,_parentGameObject.transform.forward,1.2f);
-            RaycastHit[] facingHit =Physics.RaycastAll(_raycastOrigin.position,_raycastOrigin.forward,0.8f);
             //Physics.OverlapSphere
-            yield return new WaitForSeconds(_botStepDelay);
-            print($"There are {facingHit.Length} colliders on this step");
-            for(int i = 0; i < facingHit.Length; i++)
-            {
-                if (platformCheck== false && facingHit[i].collider.gameObject.layer ==7)
-                {
-                    print(facingHit[i].collider.gameObject.layer.ToString());
-                    platformCheck = true;
-                }
-                
-                //print(i);
-                if (facingHit[i].collider.gameObject.GetComponent<WallTile>())
-                {
-                    wallTile = facingHit[i].collider.gameObject.GetComponent<WallTile>();
-                }
-                else if(wallTile == null)
-                {
-                    wallTile = null;
-                }
-                if(box == null || facingHit[i].collider.gameObject.GetComponent<PushableBox>())
-                {
-                    box = facingHit[i].collider.gameObject.GetComponent<PushableBox>();
-                }
-                  print(facingHit[i].collider.gameObject.name); 
-            }
             if (wallTile == null || (wallTile != null && wallTile.HasColision == false))
             {
                 //if (!Physics.SphereCast(transform.position, 0.3f, transform.position + new Vector3(correctedDirection.x, _goundcheckOffset, correctedDirection.z), out groundHit, _platformGroundCheckLayer))
